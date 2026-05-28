f09c7f12214c9a725cc999b43ee1499f4922e4b6
EgorShevchenko <egor.shevchenko.us@gmail.com>
2026-05-28 14:11:22 -0400

    Update login.php to use new database credentials, removed unused variables

--- backend/login.php
+++ backend/login.php
@@ -3,15 +3,9 @@
     require_once 'utils.php';
 
 	$inData = getRequestInfo();
-	
-	$ID = 0;
-	$FirstName = "";
-	$LastName = "";
-    $Email = "";
-    $Phone = "";
 
     // user does not connect directly to database, so use line below
-	$conn = new mysqli("206.81.15.115", "TheBeast", "WeLoveCOP4331", "Tables_in_COP4331"); // API connects to database using this
+	$conn = new mysqli("206.81.15.115", "alvcm", "ILoveCOP4331C", "Tables_in_COP4331"); // API connects to database using this
 	if( $conn->connect_error )
 	{
 		returnWithError( $conn->connect_error );
@@ -19,13 +13,13 @@
 	else
 	{
         $stmt = $conn->prepare("SELECT ID, FirstName, LastName FROM Users WHERE Login=? AND Password=?");
-		$stmt->bind_param("ss", $inData["Login"], $inData["Password"]);
+		$stmt->bind_param("ss", $inData["username"], $inData["password"]);
 		$stmt->execute();
 		$result = $stmt->get_result();
 
 		if( $row = $result->fetch_assoc()  )
 		{
-			returnWithInfo( $row['FirstName'], $row['LastName'], $row['ID'] );
+			returnWithLoginInfo( $row['FirstName'], $row['LastName'], $row['ID'] );
 		}
 		else
 		{
